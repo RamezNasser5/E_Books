73a10b00bbda4cebeb1f70219c1e97ec4b3c9215
RamezNasser5 <rameznasser55@gmail.com>
2024-01-23 14:27:15 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const EBook());
+}
+
+class EBook extends StatelessWidget {
+  const EBook({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
