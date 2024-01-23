e90bdac706a877cdc57596b729a931e9fb099fad
RamezNasser5 <rameznasser55@gmail.com>
2024-01-23 14:47:58 +0200

    Add Splach View

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:e_books/features/Splach/presentation/views/splach_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const EBook());
@@ -9,6 +11,8 @@ class EBook extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const GetMaterialApp(
+      home: SplachView(),
+    );
   }
 }
