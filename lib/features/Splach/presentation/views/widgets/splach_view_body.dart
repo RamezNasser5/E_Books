becbca1fb1719e557e631016f823cf459cd98544
RamezNasser5 <rameznasser55@gmail.com>
2024-01-23 22:51:40 +0200

    refactoring initState & navigate to home view

--- lib/features/Splach/presentation/views/widgets/splach_view_body.dart
+++ lib/features/Splach/presentation/views/widgets/splach_view_body.dart
@@ -1,8 +1,36 @@
+import 'package:e_books/consts.dart';
 import 'package:e_books/core/utils/assets.dart';
+import 'package:e_books/features/Splach/presentation/views/widgets/sliding_text.dart';
+import 'package:e_books/features/home/presentaion/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplachViewBody extends StatelessWidget {
+class SplachViewBody extends StatefulWidget {
   const SplachViewBody({super.key});
+
+  @override
+  State<SplachViewBody> createState() => _SplachViewBodyState();
+}
+
+class _SplachViewBodyState extends State<SplachViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initAnimationState();
+
+    navigatToHome();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +40,33 @@ class SplachViewBody extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Image.asset(AssetData().logo),
+          SlidingText(slidingAnimation: slidingAnimation),
         ],
       ),
     );
+  }
+
+  void initAnimationState() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+
+    // Start the animation when the widget is first built
+    animationController.forward();
+  }
+
+  void navigatToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        const HomeView(),
+        transition: Transition.fadeIn,
+        duration: kTransitionDuration,
+      );
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class EBook extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
       home: const SplachView(),
     );
   }
