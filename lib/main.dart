e0b357849e1b8dd9f1e8cdb7dd1444dcd14f8aad
RamezNasser5 <rameznasser55@gmail.com>
2024-01-24 10:33:55 +0200

    add google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:e_books/consts.dart';
 import 'package:e_books/features/Splach/presentation/views/splach_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const EBook());
@@ -14,7 +15,10 @@ class EBook extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+      ),
       home: const SplachView(),
     );
   }
